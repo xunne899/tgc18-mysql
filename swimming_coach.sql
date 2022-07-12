7f50ca1a24166cee34af3cf56183bca15eb22c3e
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-07-12 18:46:37 +0000

    add new content swimming coach

--- swimming_coach.sql
+++ swimming_coach.sql
@@ -48,3 +48,76 @@ create table students(
 
 alter table students add constraint students_parents
 foreign key (parent_id) references parents(parent_id);
+
+
+
+create table locations(
+  location_id int unsigned not null primary key,
+  name varchar(100) not null,
+  address varchar(255) not null
+
+)engine = innodb;
+
+
+
+
+
+create table student_session(
+  student_session_id int unsigned not null primary key,
+  student_id int unsigned not null,
+   session_id int unsigned not null
+
+)engine = innodb;
+
+
+
+alter table student_session add constraint student_session_students
+foreign key (student_id) references students(student_id);
+
+
+alter table student_session add constraint student_session_sessions
+foreign key (session_id) references sessions(session_id);
+
+create table sessions(
+  session_id int unsigned not null primary key,
+  datetime datetime not null,
+   location_id int unsigned not null
+
+)engine = innodb;
+
+
+
+alter table sessions add constraint sessions_locations
+foreign key (location_id) references locations(location_id);
+;
+
+
+
+
+create table payments(
+  payment_id int unsigned not null primary key,
+   parent_id int unsigned not null,
+   session_id int unsigned not null,
+    student_id int unsigned not null,
+    payment_mode varchar(255) not null,
+     amount float unsigned not null
+
+
+)engine = innodb;
+
+
+
+
+
+
+alter table payments add constraint payments_parents
+foreign key (parent_id) references parents(parent_id);
+
+
+alter table payments add constraint payments_sessions
+foreign key (session_id) references sessions(session_id);
+
+alter table payments add constraint payments_students
+foreign key (student_id) references students(student_id);
+
+
